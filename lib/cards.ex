973a6a5be148cf7b4bb58013bacd709e05cd3a52
Alex Sahm <sahm.alexander@pwc.com>
2019-07-25 09:57:06 +0200

    initial commit Elixit Cards project

---
+++ lib/cards.ex
@@ -0,0 +1,30 @@
+defmodule Cards do
+
+  # comprehension
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = [ "Spades", "Clubs", "Hearts", "Diamonds"]
+    for suit <- suits, value <- values do
+        "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  # method with ? in the name = returns a boolean value (like a question...Does it contain xyz? yes/no)
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def save(deck, filename) do
+    binaray = :erlang.term_to_binary(deck)
+    File.write(filename, binaray)
+  end
+
+  def deal(deck, hand_size) do
+    d = shuffle(deck)
+    Enum.split(d, hand_size)
+  end
+end
