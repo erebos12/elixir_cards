eab1b03cd28f481571c85bb105df20d26c958e0f
Alex Sahm <sahm.alexander@pwc.com>
2019-07-26 09:02:42 +0200

    added some awesome doc tests

--- lib/cards.ex
+++ lib/cards.ex
@@ -5,7 +5,7 @@ defmodule Cards do
 
   # comprehension
   @doc """
-    Returns a list of strings representing a deck of playing cards
+  Returns a list of strings representing a deck of playing cards
   """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
@@ -20,6 +20,21 @@ defmodule Cards do
   end
 
   # method with ? in the name = returns a boolean value (like a question...Does it contain xyz? yes/no)
+  @doc """
+  Determines whether a deck contains a given card
+
+  ## Examples
+
+        iex> deck = Cards.create_deck
+        ["Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades",
+         "Five of Spades", "Ace of Clubs", "Two of Clubs", "Three of Clubs",
+         "Four of Clubs", "Five of Clubs", "Ace of Hearts", "Two of Hearts",
+         "Three of Hearts", "Four of Hearts", "Five of Hearts", "Ace of Diamonds",
+         "Two of Diamonds", "Three of Diamonds", "Four of Diamonds", "Five of Diamonds"]
+        iex> Cards.contains?(deck, "Ace of Spades")
+        true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
@@ -49,8 +64,7 @@ defmodule Cards do
         ["Ace of Spades"]
   """
   def deal(deck, hand_size) do
-    d = shuffle(deck)
-    Enum.split(d, hand_size)
+    Enum.split(deck, hand_size)
   end
 
   # use consinstent FIRST argument for functions
